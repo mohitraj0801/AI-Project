0554109b1d3c38212521e23a92434ca076ea0451
mohitraj0401 <mohit.2023ug3001@iiitranchi.ac.in>
2025-04-20 00:34:20 +0530

    updated port

--- flask_web_app.py
+++ flask_web_app.py
@@ -90,5 +90,9 @@ def crop_recommendation():
         return jsonify({'error': str(e)})
 
 # --- Run the combined app ---
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT not set
+    app.run(host="0.0.0.0", port=port, debug=True)
+
